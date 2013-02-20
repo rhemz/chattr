eadbe2c278041eb225f3e41b2ee2b6261c63e87e
Russ Zeien <russ@zeien.org>
2013-02-19 23:56:27 -0500

    add create() that responds to HTTP GET

--- application/controllers/rest/room.php
+++ application/controllers/rest/room.php
@@ -2,14 +2,27 @@
 
 class Room_Controller extends Controller_Rest
 {
+	private $room_model;
+
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->room_model = new Chatroom_Model();
 	}
 
 
 	public function get_create()
 	{
+		$helper = new Chatroom_Helper();
+		$id = null;
+
+		while(!$this->room_model->create_room($id, 1)) // get userid from session
+		{
+			$id = $helper->generate_id($this->config->get('chatroom.id_length'));
+		}
+
+		Logger::log(sprintf("created room: %s", $id));
 		
 	}
 }
