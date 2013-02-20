0e8997d9bb40995af84926d5608cb813b198e391
Russ Zeien <russ@zeien.org>
2013-02-19 23:18:57 -0500

    adding room_exists()

--- application/models/chatroom.php
+++ application/models/chatroom.php
@@ -8,5 +8,17 @@ class Chatroom_Model extends Model
 		parent::__construct();
 	}
 
-	
+
+	public function room_exists($room_id)
+	{
+		$sql = "SELECT id FROM room WHERE id = ?";
+		if($this->query($sql, array($room_id)))
+		{
+			$result = $this->result();
+			return ($result->num_rows() > 0);
+		}
+		return true; // if query fails, say room exists so as to not try to create duplicate
+	}
+
+
 }
