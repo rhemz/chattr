4edbacc3c595f878e477a742609a425a1a00d85f
Russ Zeien <russ@zeien.org>
2013-02-19 23:04:22 -0500

    creating empty models & REST controllers

---
+++ application/controllers/rest/room.php
@@ -0,0 +1,15 @@
+<?php
+
+class Room_Controller extends Controller_Rest
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	public function get_create()
+	{
+		
+	}
+}
---
+++ application/models/chatroom.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Chatroom_Model extends Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	
+}
