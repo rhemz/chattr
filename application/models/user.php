194a71a168fa4eaebb73d82017ac2765eb6f1ade
Russ Zeien <russ@zeien.org>
2013-02-20 19:00:32 -0500

    user mode, get_id() and create_user() added

--- application/models/user.php
+++ application/models/user.php
@@ -7,5 +7,27 @@ class User_Model extends Model
 		parent::__construct();
 	}
 
+
+	public function get_id($session_id)
+	{
+		$sql = "SELECT id FROM user WHERE session_id = ?";
+		if($this->query($sql, array($session_id)))
+		{
+			$result = $this->result();
+			return $result->num_rows() > 0 ? $result->rows[0]->id : null;
+		}
+		return false;
+	}
+
+
+	public function create_user($session_id)
+	{
+		$sql = "INSERT INTO user (`session_id`) VALUES (?)";
+		if($this->query($sql, array($session_id)))
+		{
+			return $this->last_insert_id();
+		}
+		return false;
+	}
 	
 }
