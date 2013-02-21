ae87846af319879d56e22b70b9eee64632f5c2ea
Russ Zeien <russ@zeien.org>
2013-02-20 19:01:40 -0500

    added create chatroom functionality

--- application/controllers/rest/room.php
+++ application/controllers/rest/room.php
@@ -2,27 +2,29 @@
 
 class Room_Controller extends Controller_Rest
 {
+	private $user;
+
 	private $room_model;
 
 	public function __construct()
 	{
 		parent::__construct();
 
+		$this->user = new Session_User();
 		$this->room_model = new Chatroom_Model();
 	}
 
 
-	public function get_create()
+	public function post_create()
 	{
 		$helper = new Chatroom_Helper();
 		$id = null;
 
-		while(!$this->room_model->create_room($id, 1)) // get userid from session
+		while(!$this->room_model->create_room($id, $this->user->get_id()))
 		{
 			$id = $helper->generate_id($this->config->get('chatroom.id_length'));
 		}
 
-		Logger::log(sprintf("created room: %s", $id));
-		
+		Output::return_json(array('id' => $id));	
 	}
 }
