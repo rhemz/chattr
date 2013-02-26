fa605af2d2c8a7beecfc7294484d3051e0a9492d
Russ Zeien <russ@zeien.org>
2013-02-25 20:50:25 -0500

    home view uses common views

--- application/views/home.php
+++ application/views/home.php
@@ -1,15 +1,15 @@
-<html>
-<head>
-	<title>Welcome to Chattr</title>
-</head>
-
-<body>
-
-	<input></input>
+<?php $this->load_view('common/header', array('title' => 'Welcome!')); // pass any more data you need to here w/ additional key/value pairs?>
 
 	<p>
-		<?=$something?>
+	A button or something should go here to create a chatroom.  It should make an empty ajax HTTP POST to /rest/room/create
 	</p>
 
-</body>
-</html>
+
+	<p>
+		Your user_id: <?=$user->get_id()?>
+	</p>
+
+
+
+
+<?php $this->load_view('common/footer'); ?>
