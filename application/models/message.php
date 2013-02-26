2c9f0e46f3452da664fb968f53184bc82283600b
Russ Zeien <russ@zeien.org>
2013-02-25 20:49:58 -0500

    added get/save message functionality

--- application/models/message.php
+++ application/models/message.php
@@ -8,12 +8,52 @@ class Message_Model extends Model
 	}
 
 
-	public function save_message($user_id, $room_id, $text, $timestamp)
+	public function save_message($user_id, $room_id, $text)
 	{
-		$sql = "INSERT INTO message (`user_id`, `room`, `text`, `timestamp`) VALUES (?, ?, ?, ?)";
+		$timestamp = microtime(true);
+		$sql = "INSERT INTO message (`user_id`, `room_id`, `text`, `timestamp`) VALUES (?, ?, ?, ?)";
 		if($this->query($sql, array($user_id, $room_id, $text, $timestamp)))
 		{
 			return $this->last_insert_id();
+		}
+		return false;
+	}
+
+
+	public function get_messages($room_id, $user_id)
+	{
+		$sql = "SELECT
+					message.id as message_id,
+					message.user_id as user_id,
+					user.name as user_name,
+					message.text as text,
+					message.timestamp as timestamp
+				FROM
+					message
+				INNER JOIN
+					user ON message.user_id = user.id
+				INNER JOIN
+					message_retrieve ON message.user_id = message_retrieve.user_id
+				WHERE
+					message.room_id = ?
+				AND
+					message.timestamp >= (SELECT last_checked FROM message_retrieve WHERE user_id = ?)";
+
+		if($this->query($sql, array($room_id, $user_id)))
+		{
+			$result = $this->result();
+
+			$timestamp = microtime(true);
+
+			$sql = "INSERT INTO 
+						message_retrieve (`user_id`, `room_id`, `last_checked`) VALUES (?, ?, ?)
+					ON DUPLICATE KEY 
+						UPDATE `last_checked` = ?";
+
+			if($this->query($sql, array($user_id, $room_id, $timestamp, $timestamp)))
+			{
+				return $result;
+			}
 		}
 		return false;
 	}
