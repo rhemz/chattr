1eeb126d3872df484a3134a3bc2f4db12269e8a6
Russ Zeien <russ@zeien.org>
2013-02-25 20:50:09 -0500

    chatroom view

--- application/views/chat.php
+++ application/views/chat.php
@@ -1,31 +1,30 @@
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Welcome to: <?=$progName?></title>
-	<link rel="stylesheet" type="text/css" href="../main.css"/>    
-	<script src="/public/scripts/jquery.min.js" type="text/javascript"></script>
-	<script src="/public/scripts/chat.js" type="text/javascript" ></script>
-	<script type="text/javascript" src="settings.js"></script>
-</head>
-<body>
-	<div id="page-wrap"> 
-		<div id="header">
-			<div id="current-user"><span>Logged in as:</span> <?=$userName?></div>
-		</div>
-		<div id="section">
-			<h2><?=$userName?></h2>
-			<div id="chat"></div>
-			<div id="userlist">
-				<ul>
-				<?php foreach($users as $user): ?>
-					<li><?=$user->name?></li>
-				<?php endforeach; ?>
-				</ul>
-			</div>
-			<form id="chat-form">
-				<textarea maxlength='144'></textarea>
-			</form>
-		</div>
+<?php $this->load_view('common/header', array('title' => 'Chatting.  Room name will eventually go here')); ?>
+
+<div id="page-wrap"> 
+	<div id="header">
+		<div id="current-user"><span>Logged in as:</span> <?=$userName?></div>
 	</div>
-</body>
-</html>
+	<div id="section">
+		<h2><?=$userName?></h2>
+		<div id="chat"></div>
+		<div id="userlist">
+			<ul>
+			<?php foreach($users as $user): ?>
+				<li><?=$user->name?></li>
+			<?php endforeach; ?>
+			</ul>
+		</div>
+		<form id="chat-form">
+			<textarea maxlength='144'></textarea>
+		</form>
+	</div>
+</div>
+
+
+make ajax GET requests to /rest/room/room_id/messages
+<br />
+make ajax POST request to /rest/message/send, sending keys 'text' 'room_id'
+<br />
+make ajax GET request to /rest/room/room_id
+
+<?php $this->load_view('common/footer'); ?>
