f68d5c0b6b6d56740a335e38bfd2f61e462b696e
Russ Zeien <russ@zeien.org>
2013-02-25 20:49:11 -0500

    added room function

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,15 +2,41 @@
 
 class Home_Controller extends Controller
 {
+	private $user;
+
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->user = new Session_User();
 	}
 
 
 	public function index()
 	{
-		// go from here!
-		$this->load_view('home', array('something' => "I'm some data from the controller!"));
+		$this->load_view('home', array('user' => $this->user));
+	}
+
+
+	public function chatroom($room_id)
+	{
+		// just load the room view and make the user & room ID accessible to it.
+		// all other data to be loaded w/ ajax calls from the REST controllers
+
+		$rooms = new Chatroom_Model();
+
+		if($rooms->room_exists($room_id))
+		{
+			$this->load_view('chat', array('user' => $this->user, 'room_id' => $room_id));
+		}
+		else
+		{
+			// load the 404 view
+			$this->load_view('404', array(
+				'title' => 'Room not found', 
+				'message' => "Sorry, the chatroom you're looking for does not exist"));
+		}
+
+		
 	}
 }
