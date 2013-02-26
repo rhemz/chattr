fa731a52f714df7c93414d4f52951b2c0a29f8a6
Russ Zeien <russ@zeien.org>
2013-02-25 20:48:26 -0500

    new chatroom config values

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -3,3 +3,7 @@
 
 $config['id_length']				= 16;
 $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+
+
+$config['time_leave']				= 30; // if no message checks comes from client for 30s, assume they left/closed room
+$config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
