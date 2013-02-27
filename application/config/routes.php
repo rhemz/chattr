3594c86e96d3fd7cdff59800d8726317b260ea36
Russ Zeien <russ@zeien.org>
2013-02-26 20:41:40 -0500

    changed message/send to /room/ID#/message POST

--- application/config/routes.php
+++ application/config/routes.php
@@ -20,6 +20,7 @@ $config['mappings'] = array(
 	'/room/%var'					=> '/home/chatroom/$1',
 
 	// RESTful routes
-	'/rest/room/%var/messages'		=> '/rest/room/messages/$1',
-	'/rest/room/%var/send'			=> '/rest/message/send'
+	'/rest/room/%var/messages'		=> '/rest/room/messages/$1', // GET new messages for room
+	'/rest/room/%var/send'			=> '/rest/message/send', // POST (send) a message to the room
+	'/rest/room/%var/leave'			=> '/rest/room/leave/$1' // GET leave the room
 );
