fb11b63fdab308cdc604109997188d5be51c1cf5
Russ Zeien <russ@zeien.org>
2013-03-19 17:28:27 -0400

    added create room functionality to view

--- application/views/home.php
+++ application/views/home.php
@@ -1,13 +1,37 @@
 <?php $this->load_view('common/header', array('title' => 'Welcome!')); // pass any more data you need to here w/ additional key/value pairs?>
 
-	<p>
-	A button or something should go here to create a chatroom.  It should make an empty ajax HTTP POST to /rest/room/create
-	</p>
+<script type="text/javascript">
+
+$(document).ready(function() {
+
+	$("#createRoom").click(function(e) {
+		$.ajax({
+			url: "/rest/room/create",
+			cache: false,
+			type: "POST"	
+		}).done(function(response) {
+			var obj = jQuery.parseJSON(response);
+			if(obj.success) {
+				// alert("Successfully created roomID: " + obj.id);
+
+				// display some kind of fancy feedback that indicates success and take the user to the room page
+				window.location.href = "/room/" + obj.id; 
+			} else {
+				alert("Something went horribly, horribly wrong!");
+			}
+		});
+	});
+
+});
+
+</script>
+
+<input type="button" id="createRoom" value="Create a Chatroom" />
 
 
-	<p>
-		Your user_id: <?=$user->get_id()?>
-	</p>
+<p>
+	Your user_id: <?=$user->get_id()?>
+</p>
 
 
 
