218125fc6ff111116b1690549a976ecd63f1efea
Russ Zeien <russ@zeien.org>
2013-03-19 23:27:55 -0400

    send room id as arg

--- application/config/routes.php
+++ application/config/routes.php
@@ -21,6 +21,6 @@ $config['mappings'] = array(
 
 	// RESTful routes
 	'/rest/room/%var/messages'		=> '/rest/room/messages/$1', // GET new messages for room
-	'/rest/room/%var/send'			=> '/rest/message/send', // POST (send) a message to the room
+	'/rest/room/%var/send'			=> '/rest/message/send/$1', // POST (send) a message to the room
 	'/rest/room/%var/leave'			=> '/rest/room/leave/$1' // GET leave the room
 );
