7e9bdcdcb8c4d68b47f8434c1826750b2fdf9787
Russ Zeien <russ@zeien.org>
2013-03-20 18:56:59 -0400

    adding username support

--- application/models/user.php
+++ application/models/user.php
@@ -20,14 +20,21 @@ class User_Model extends Model
 	}
 
 
-	public function create_user($session_id)
+	public function create_user($session_id, $username)
 	{
-		$sql = "INSERT INTO user (`session_id`) VALUES (?)";
-		if($this->query($sql, array($session_id)))
+		$sql = "INSERT INTO user (`session_id`, `name`) VALUES (?, ?)";
+		if($this->query($sql, array($session_id, $username)))
 		{
 			return $this->last_insert_id();
 		}
 		return false;
+	}
+
+
+	public function set_name($user_id, $name)
+	{
+		$sql = "UPDATE user SET `name` = ? WHERE id = ?";
+		return $this->query($sql, array($name, $user_id));
 	}
 	
 }
