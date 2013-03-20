e6031a27cc846e6d3545f8678b1412d15b767b0c
Russ Zeien <russ@zeien.org>
2013-03-20 19:06:51 -0400

    increase message check interval

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -9,8 +9,8 @@ $config['time_leave']				= 30; // if no message checks comes from client for 30s
 $config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
 
 
-$config['client_check_interval']	= 3000; // 1s.  to be twerked.
-$config['client_check_timeout']		= 3000;
+$config['client_check_interval']	= 2000; // 1s.  to be twerked.
+$config['client_check_timeout']		= 2600;
 
 $config['client_send_interval']		= 100; // allow no more than 10 messages to be sent per second
 $config['client_send_timeout']		= 5000;
