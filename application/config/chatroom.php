b70a5064552a5a8dd550b8638048813d0989f720
Russ Zeien <russ@zeien.org>
2013-03-20 18:56:47 -0400

    added client intervals and timeouts

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -7,3 +7,10 @@ $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1
 
 $config['time_leave']				= 30; // if no message checks comes from client for 30s, assume they left/closed room
 $config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
+
+
+$config['client_check_interval']	= 3000; // 1s.  to be twerked.
+$config['client_check_timeout']		= 3000;
+
+$config['client_send_interval']		= 100; // allow no more than 10 messages to be sent per second
+$config['client_send_timeout']		= 5000;
