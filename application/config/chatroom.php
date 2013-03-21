22cf24aa13fb173ca33d8251647c66b93e9e20d7
Russ Zeien <russ@zeien.org>
2013-03-21 15:40:57 -0400

    adding more check intervals

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -5,12 +5,15 @@ $config['id_length']				= 16;
 $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 
 
-$config['time_leave']				= 30; // if no message checks comes from client for 30s, assume they left/closed room
-$config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
+$config['time_leave']				= 10; // if no message checks comes from client for 10s, assume they left/closed room
+$config['time_dead']				= 60*30; // 30 minutes of no message checks, room is dead
 
 
-$config['client_check_interval']	= 2000; // 1s.  to be twerked.
-$config['client_check_timeout']		= 2600;
+$config['message_check_interval']	= 2000; // 1s.  to be twerked.
+$config['message_check_timeout']	= 2600;
 
-$config['client_send_interval']		= 100; // allow no more than 10 messages to be sent per second
-$config['client_send_timeout']		= 5000;
+$config['message_send_interval']	= 100; // allow no more than 10 messages to be sent per second
+$config['message_send_timeout']		= 5000;
+
+$config['room_check_interval']		= 5000;
+$config['room_check_timeout']		= 2000;
