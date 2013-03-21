a6d059326aacb787a537dd6d9aa165d1228d9f5a
Russ Zeien <russ@zeien.org>
2013-03-21 15:41:52 -0400

    changing post_name to put_name()

--- application/controllers/rest/user.php
+++ application/controllers/rest/user.php
@@ -8,10 +8,10 @@ class User_Controller extends Controller_Rest
 	}
 
 
-	public function post_name()
+	public function put_name()
 	{
 		$user = new Session_User();
 
-		Output::return_json(array('success' => $user->set_name(Input::post('username'))));
+		Output::return_json(array('success' => $user->set_name(Input::put('username'))));
 	}
 }
