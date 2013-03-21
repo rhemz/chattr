ebd9b13ca182d6a97aa68b671c87346e96728975
Russ Zeien <russ@zeien.org>
2013-03-21 17:05:42 -0400

    lowering intervals

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -5,15 +5,15 @@ $config['id_length']				= 16;
 $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 
 
-$config['time_leave']				= 10; // if no message checks comes from client for 10s, assume they left/closed room
+$config['time_leave']				= 7; // if no message checks comes from client for 7s, assume they left/closed room
 $config['time_dead']				= 60*30; // 30 minutes of no message checks, room is dead
 
 
-$config['message_check_interval']	= 2000; // 1s.  to be twerked.
-$config['message_check_timeout']	= 2600;
+$config['message_check_interval']	= 1500; // 1.5s.  to be twerked.
+$config['message_check_timeout']	= 2000;
 
 $config['message_send_interval']	= 100; // allow no more than 10 messages to be sent per second
 $config['message_send_timeout']		= 5000;
 
-$config['room_check_interval']		= 5000;
+$config['room_check_interval']		= 4000;
 $config['room_check_timeout']		= 2000;
