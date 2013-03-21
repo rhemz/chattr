926da4483d1026a7d73757018df5ab8dd8ed9c3d
Russ Zeien <russ@zeien.org>
2013-03-21 15:41:08 -0400

    adding users REST route

--- application/config/routes.php
+++ application/config/routes.php
@@ -21,6 +21,7 @@ $config['mappings'] = array(
 
 	// RESTful routes
 	'/rest/room/%var/messages'		=> '/rest/room/messages/$1', // GET new messages for room
+	'/rest/room/%var/users'			=> '/rest/room/users/$1', // GET the current users in the room
 	'/rest/room/%var/send'			=> '/rest/message/send/$1', // POST (send) a message to the room
 	'/rest/room/%var/leave'			=> '/rest/room/leave/$1' // GET leave the room
 );
