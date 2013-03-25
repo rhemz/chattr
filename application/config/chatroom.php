be2fb6a08dd93de5a2b5d80deaa4feb1c1930cce
Russ Zeien <russ@zeien.org>
2013-03-25 18:27:15 -0400

    new chatroom config values

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -6,7 +6,7 @@ $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1
 
 
 $config['time_leave']				= 7; // if no message checks comes from client for 7s, assume they left/closed room
-$config['time_dead']				= 60*30; // 30 minutes of no message checks, room is dead
+$config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
 
 
 $config['message_check_interval']	= 800; // 800ms.  to be twerked.
@@ -17,3 +17,12 @@ $config['message_send_timeout']		= 5000;
 
 $config['room_check_interval']		= 3500;
 $config['room_check_timeout']		= 2000;
+
+$config['notification_title']		= 'New Message';
+$config['notification_path']		= '/public/images/notification.png';
+$config['notification_timeout']		= 8000;
+$config['notification_cookie']		= 'chattr_notify';
+
+$config['title_blink']				= true;
+$config['title_blink_count']		= 10;
+$config['title_blink_delay']		= 1000;
