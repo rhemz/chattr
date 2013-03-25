df9273e64ebf0187963f3621e0b047f5cb83316a
Russ Zeien <russ@zeien.org>
2013-03-25 18:25:37 -0400

    new Message class

---
+++ application/classes/message.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Message
+{
+	public $id;
+	public $text;
+	public $user_id;
+	public $user_name;
+
+	public function __construct($row)
+	{
+		$this->id = $row['id'];
+		$this->text = $row['text'];
+		$this->user_id = $row['user_id'];
+		$this->user_name = $row['user_name'];
+	}
+
+
+	public function sanitize()
+	{
+		
+	}
+
+
+	public function __toString()
+	{
+		return 
+	}
+}
