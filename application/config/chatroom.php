d13b29ec761c263b53bd3d15404b349e9394d428
Russ Zeien <russ@zeien.org>
2013-04-02 19:07:41 -0400

    fixing timings

--- application/config/chatroom.php
+++ application/config/chatroom.php
@@ -1,7 +1,5 @@
 <?php
 
-$throttle = 3;
-
 $config['id_length']				= 16;
 $config['id_chars'] 				= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 
@@ -10,14 +8,14 @@ $config['time_leave']				= 7; // if no message checks comes from client for 7s, 
 $config['time_dead']				= 60*10; // 10 minutes of no message checks, room is dead
 
 
-$config['message_check_interval']	= 800 * $throttle; // 800ms.  to be twerked.
-$config['message_check_timeout']	= 2000 * $throttle;
+$config['message_check_interval']	= 800; // 800ms.  to be twerked.
+$config['message_check_timeout']	= 2000;
 
-$config['message_send_interval']	= 100 * $throttle; // allow no more than 10 messages to be sent per second
-$config['message_send_timeout']		= 5000 * $throttle;
+$config['message_send_interval']	= 100; // allow no more than 10 messages to be sent per second
+$config['message_send_timeout']		= 5000;
 
-$config['room_check_interval']		= 3500 * $throttle;
-$config['room_check_timeout']		= 2000 * $throttle;
+$config['room_check_interval']		= 3500;
+$config['room_check_timeout']		= 2000;
 
 $config['notification_title']		= 'New Message';
 $config['notification_path']		= '/public/images/notification.png';
