3ca97fc7cde93966749017ad453a6affd3f43107
Richard Carhart <rickdmer+github@gmail.com>
2013-04-07 22:36:41 -0400

    Updating meta tag for mobile devices

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 <title>Chattr - <?=$title?></title>
 
 <?php
