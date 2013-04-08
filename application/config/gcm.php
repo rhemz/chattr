4aa02e0c0c19ff62f38e0a33e0780be98357fe06
Russ Zeien <russ@zeien.org>
2013-04-08 09:44:32 -0400

    adding google cloud messaging config

---
+++ application/config/gcm.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['sender_id']				= 526326193908;
+$config['simple_api_key']			= 'AIzaSyBo0PfSobqE51gyj2divr37f8bORaiLMQM';
+$config['auth_api_key']				= 'R0dAIgsDN9NAgizhhFZ1i9tY';
+$config['client_id']				= '526326193908.apps.googleusercontent.com';
+$config['redirect_uri']				= 'http://www.twotonlobster.com/oauth2callback';
