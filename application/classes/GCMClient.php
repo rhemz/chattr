c8a025e9c125bc3e21662b8112e82b9d15468ab7
Russ Zeien <russ@zeien.org>
2013-04-10 00:43:23 -0400

    basic google cloud messaging client

---
+++ application/classes/GCMClient.php
@@ -0,0 +1,66 @@
+<?php
+
+class GCM_Client
+{
+	const GCM_Send_URI = 'https://android.googleapis.com/gcm/send';
+
+	private $api_key;
+	private $registration_ids = array();
+	private $message;
+
+	private $curl;
+	private $response;
+
+
+	public function __construct($api_key)
+	{
+		$this->api_key = $api_key;
+
+		$this->curl = curl_init();
+		curl_setopt($this->curl, CURLOPT_URL, self::GCM_Send_URI);
+		curl_setopt($this->curl, CURLOPT_POST, true);
+	}
+
+	public function add_recipient($recipients)
+	{
+		if(!is_array($recipients))
+		{
+			$recipients = array($recipients);
+		}
+
+		array_push($this->registration_ids, $recipients);
+	}
+
+	public function set_message($message)
+	{
+		$this->message = $message;
+	}
+
+
+	public function send()
+	{
+		$fields = array(
+			'registration_ids'	=> $this->registration_ids,
+			'data'				=> array('message'	=> $this->message)
+		);
+
+		$headers = array(
+			'Authorization: key=' . $this->api_key,
+			'Content-Type: application/json'
+		);
+
+		curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($fields));
+
+		$this->response = curl_exec($this->curl);
+		return $this->response;
+	}
+
+
+	private function __destruct()
+	{
+		curl_close($this->curl);
+	}
+
+}
