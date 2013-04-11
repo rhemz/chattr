f518dc73d1f3997f7afb9b796abcd7236da91da0
Russ Zeien <russ@zeien.org>
2013-04-10 23:06:05 -0400

    adding change-username validation

--- application/config/user.php
+++ application/config/user.php
@@ -3,3 +3,4 @@
 
 $config['default_username']			= 'Anonymous';
 $config['default_username_max']		= 999999;
+$config['username_min_length']		= 3;
--- application/controllers/rest/user.php
+++ application/controllers/rest/user.php
@@ -12,6 +12,14 @@ class User_Controller extends Controller_Rest
 	{
 		$user = new Session_User();
 
-		Output::return_json(array('success' => $user->set_name(Input::put('username'))));
+		if(strlen($u = trim(Input::put('username'))) >= $this->config->get('user.username_min_length'))
+		{
+			Output::return_json(array('success' => $user->set_name($u)));
+		}
+		else
+		{
+			Output::return_json(array('success' => false, 'message' => 'Invalid username'));
+		}
+		
 	}
 }
