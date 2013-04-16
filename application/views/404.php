5aff7a2d1e6a56d32a10908f9e9162bfca35cdf6
Richard Carhart <rickdmer+github@gmail.com>
2013-04-16 00:42:50 -0400

    Styled 404 page

--- application/views/404.php
+++ application/views/404.php
@@ -1,5 +1,21 @@
 <?php $this->load_view('common/header', array('title' => $title)); ?>
-
-<h2><?=$message?></h2>
-
+<?php $this->load_view('js/home', array('user' => $user)); ?>
+<div class="colorbar"></div>
+<div id="content">
+	<div id="topPane">
+		<div class="left"><span class="logo no-select">Chattr</span><span class="sublogo no-select">beta</span></div>
+	</div>
+	<div class="centerwrapper">
+		<div class="cell">
+			<div class="homecontent">
+				<div class="titlecopy">
+					<p><?=$message?></p>
+				</div>
+				<div class="mainbuttondiv">
+					<button class="mainbutton no-select">Create New Chat</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php $this->load_view('common/footer'); ?>
